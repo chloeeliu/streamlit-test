79b2c3ce41faba0b7709772e5ab4fad5b072e293
chloeeliu <liuxinchi9@gmail.com>
2023-01-16 15:54:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,30 +59,3 @@ df_log_all=log_stat('day','all')
 fig = px.line(df_log_all,x='date_',y='操作人_nunique')
 st.plotly_chart(fig, use_container_width=True)
 
-
-'''
-
-df = sns.load_dataset('titanic')
-
-st.title('ETP使用数据')
-st.subheader('总体日活')
-st.dataframe(df)
-
-st.subheader('总体月活')
-st.dataframe(df.describe())
-
-st.subheader('Data Visualization with respect to Survived')
-left_column, right_column = st.columns(2)
-with left_column:
-   'Numerical Plot'
-   num_feat = st.selectbox(
-   'Select Numerical Feature', df.select_dtypes('number').columns)
-   fig = px.histogram(df, x = num_feat, color = 'survived')
-   st.plotly_chart(fig, use_container_width=True)
-with right_column:
-   'Categorical column'
-   cat_feat = st.selectbox(
-    'Select Categorical Feature', df.select_dtypes(exclude =   'number').columns)
-   fig = px.histogram(df, x =cat_feat, color = 'survived' )
-   st.plotly_chart(fig, use_container_width=True)
-   '''
